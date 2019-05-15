f4f9fa99b615cc16f2a9b075329459ac21955834
Michael Green <mgreen1980@gmail.com>
2019-05-15 15:35:53 -0400

    ...

--- reverse-string/reverse_string.cpp
+++ reverse-string/reverse_string.cpp
@@ -1,9 +1,5 @@
 #include "reverse_string.h"
 
-std::string reverse_string::reverse_string(std::string str) {
-    std::string result;
-    for (int i = str.length() - 1; i >= 0; --i) {
-        result += str.at(i);
-    }
-    return result;
+std::string reverse_string::reverse_string(const std::string& str) {
+    return std::string(str.rbegin(), str.rend());
 }
--- reverse-string/reverse_string.h
+++ reverse-string/reverse_string.h
@@ -3,5 +3,5 @@
 #define EXERCISM_RUN_ALL_TESTS true
 
 namespace reverse_string {
-    std::string reverse_string(std::string str);
+    std::string reverse_string(const std::string& str);
 }
